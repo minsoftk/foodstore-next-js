2188ac7961b506b666ac21511ff6c7d916aa9864
Minsung Kim <minsoftk@gmail.com>
2021-09-06 04:42:11 +0900

    code 부분 css 수정

--- pages/store.js
+++ pages/store.js
@@ -9,25 +9,9 @@ import StoreModal from '../components/Modal';
 import styles from './store.module.css';
 
 /** rest api 데이터 호출 **/
-// export async function getStaticProps() {
-// 	const res = await fetch(`http://localhost:9000/stores`);
-// 	const data = await res.json();
-// 	if (!data) {
-// 		return {
-// 			notFound: true,
-// 		};
-// 	}
-// 	return {
-// 		props: { data }, // will be passed to the page component as props
-// 	};
-// }
-
-/** 배포용 **/
 export async function getStaticProps() {
-	const res = await fetch(`https://minsoftk.github.io/jsontest/db.json`);
-	let data = await res.json();
-	data = data.stores;
-	console.log(data);
+	const res = await fetch(`http://localhost:9000/stores`);
+	const data = await res.json();
 	if (!data) {
 		return {
 			notFound: true,
@@ -37,6 +21,22 @@ export async function getStaticProps() {
 		props: { data }, // will be passed to the page component as props
 	};
 }
+
+/** 배포용 **/
+// export async function getStaticProps() {
+// 	const res = await fetch(`https://minsoftk.github.io/jsontest/db.json`);
+// 	let data = await res.json();
+// 	data = data.stores;
+// 	console.log(data);
+// 	if (!data) {
+// 		return {
+// 			notFound: true,
+// 		};
+// 	}
+// 	return {
+// 		props: { data }, // will be passed to the page component as props
+// 	};
+// }
 
 /** getStaticProps를 통해서 Store 컴포넌트에 props로 데이터 전달 **/
 const Store = (props) => {
