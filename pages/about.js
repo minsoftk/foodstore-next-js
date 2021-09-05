5c277ce2f40d1bc8865622df6736eeefd0e6ff06
Minsung Kim <minsoftk@gmail.com>
2021-09-06 03:28:45 +0900

    about 페이지 css 네이밍 수정

--- pages/about.js
+++ pages/about.js
@@ -15,20 +15,29 @@ const About = () => {
 			</Head>
 
 			<div>
-				<p className={styles.title}>About</p>
+				<p className={styles.main_title}>About</p>
 				<hr></hr>
 			</div>
 
-			<div className={styles.container}>
-				<main className={styles.main}>
-					<div className={styles.grid}>
-						<h1 className={styles.btitle}>Food Store Project</h1>
-						<p className={styles.description}>
+			<div className={styles.about_ontainer}>
+				<main className={styles.about_main}>
+					<div className={styles.about_grid}>
+						<h1 className={styles.sub_title}>Food Store Project</h1>
+						<p className={styles.about_project_description}>
 							React 프레임워크 Next.js를 사용해서 Store 페이지 개발하기.
+							<br />
+							<br />
+							Next.js 쓰는 이유
+							<br />
+							1. 모든 페이지를 Pre-rendering한다. getStaticProps를 활용해,
+							렌더링 전에 data를 가져오는 것도 가능.
+							<br />
+							2. Head 태그를 이용해 SEO 최적화
+							<br />
+							3. Image 태그를 이용한 이미지 최적화. lazy-loading default.
+							<br />
+							4. 코드 스플리팅 자동화
 						</p>
-						<div>1. 내장된 Link를 사용한 pre-rendering 기능</div>
-						<div>2. 페이지들마다 meta 태그를 입력해 SEO 최적화</div>
-						<div>3. </div>
 					</div>
 				</main>
 			</div>
