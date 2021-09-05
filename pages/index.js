49206ada51c58e4935375c0a3d3612d1912b8662
Minsung Kim <minsoftk@gmail.com>
2021-09-06 03:03:46 +0900

    index css 네이밍

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ import mainpicture from '../public/img/italiancuisine.jpg';
 
 const Home = () => {
 	return (
-		<div className={styles.container}>
+		<>
 			<Head>
 				<title>AWESOME FOOD STORE</title>
 				<meta name="viewport" content="initial-scale=1, width=device-width" />
@@ -19,33 +19,35 @@ const Home = () => {
 				<meta httpEquiv="Title" content="맛집 리스트 메인페이지" />
 			</Head>
 
-			<main className={styles.main}>
-				<Image
-					className={styles.img}
-					src={mainpicture}
-					alt="main-food-picture"
-				/>
-				<h1 className={styles.title}>Awesome food!</h1>
+			<div className={styles.container}>
+				<main className={styles.main}>
+					<Image
+						className={styles.main_image}
+						src={mainpicture}
+						alt="main-food-picture"
+					/>
+					<h1 className={styles.main_title}>Awesome food!</h1>
 
-				<p className={styles.description}>
-					Let's find a good restaurant. go to &rarr;{' '}
-					<Link href="/store" scroll={false}>
-						<code className={styles.code}>Store page!</code>
-					</Link>
-				</p>
-				<p className={styles.descriptionStore}>
-					200개의 컨테이너로 구성된 국내 최대 컨테이너 팝업 쇼핑몰
-					커먼그라운드에서는
-					<br />
-					색다른 쇼핑의 재미와 다양한 미식 브랜드들을 만나보실 수 있습니다.
-					<br />
-					젊고 유니크한 CULTURE를 향유하는 YOUTH를 위한 복합문화공간,
-					<br />
-					커먼그라운드에서 기존의 쇼핑몰에서 느낄 수 없었던 다채로운 즐거움을
-					경험해보세요.
-				</p>
-			</main>
-		</div>
+					<p className={styles.sub_title}>
+						Let's find a good restaurant. go to &rarr;{' '}
+						<Link href="/store" scroll={false}>
+							<code className={styles.code}>Store page!</code>
+						</Link>
+					</p>
+					<p className={styles.main_description}>
+						200개의 컨테이너로 구성된 국내 최대 컨테이너 팝업 쇼핑몰
+						커먼그라운드에서는
+						<br />
+						색다른 쇼핑의 재미와 다양한 미식 브랜드들을 만나보실 수 있습니다.
+						<br />
+						젊고 유니크한 CULTURE를 향유하는 YOUTH를 위한 복합문화공간,
+						<br />
+						커먼그라운드에서 기존의 쇼핑몰에서 느낄 수 없었던 다채로운 즐거움을
+						경험해보세요.
+					</p>
+				</main>
+			</div>
+		</>
 	);
 };
 export default Home;
