8545d01a71eecf47366eae40bce0fc5d2f4974d1
Minsung Kim <minsoftk@gmail.com>
2021-09-08 22:44:49 +0900

    Timer 해제 로직 추가

--- pages/404.js
+++ pages/404.js
@@ -15,9 +15,16 @@ const Custom404 = () => {
 
 	//404페이지로 접속 되었을 때, 5초후 메인페이지로 이동
 	useEffect(() => {
-		setTimeout(() => {
+		let Timer = setTimeout(() => {
 			router.replace('/');
 		}, 5000);
+
+		//5초마다 반복되는 Timer 해제
+		//Timer가 비동기로 처리된 뒤 마운트 해제될 때, cleartimeout 요청
+		const clearTimer = () => {
+			clearTimeout(Timer);
+		};
+		return clearTimer();
 	}, []);
 
 	//5초 카운트 다운 로직
